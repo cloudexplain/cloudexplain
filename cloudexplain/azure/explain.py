d2971cfff7c1099e1e2aad468c77e9094a7f0f9d
Tobias Pitters <tobias.pitters@gmail.com>
2024-11-13 10:00:42 +0100

    allow to specify resource group

--- cloudexplain/azure/explain.py
+++ cloudexplain/azure/explain.py
@@ -35,12 +35,13 @@ def get_subscription_id(credentials):
         if subscription.state == 'Enabled':
             return subscription.subscription_id
 
-def _find_cloudexplain_resource_group(credentials):
+def _find_cloudexplain_resource_group(credentials, resource_group_name: str = None):
     subscription_id = get_subscription_id(credentials=credentials)
     client = ResourceManagementClient(credentials, subscription_id=subscription_id)
+    pattern_to_search = resource_group_name or NAME_PATTERN_RG
 
     for item in client.resource_groups.list():
-        if NAME_PATTERN_RG in item.name:
+        if pattern_to_search in item.name:
             return item
 
 def _find_cloudexplain_storage_acc(subscription_id: str, credentials, cloudexplain_rg: str):
@@ -58,7 +59,7 @@ def _get_data_container_client_from_account(credentials, account: str):
     container_client = blob_service_client.get_container_client(CONTAINER_NAME)
     return container_client
 
-def get_data_container_client() -> BlobServiceClient:
+def get_data_container_client(resource_group_name: str | None) -> BlobServiceClient:
     """Get the container client for the cloudexplaindata container.
 
     Returns:
@@ -66,7 +67,7 @@ def get_data_container_client() -> BlobServiceClient:
     """
     credentials = AzureCliCredential()
 
-    cloudexplain_rg = _find_cloudexplain_resource_group(credentials=credentials)
+    cloudexplain_rg = _find_cloudexplain_resource_group(credentials=credentials, resource_group_name=resource_group_name)
     subscription_id = get_subscription_id(credentials=credentials)
 
     account = _find_cloudexplain_storage_acc(subscription_id=subscription_id, credentials=credentials, cloudexplain_rg=cloudexplain_rg)
@@ -83,13 +84,13 @@ async def _upload_blobs_async(container_client, directory_name, X, y, model, mod
 
 
 class ExplainModelContext:
-    def __init__(self, model, X, y, model_version=None, model_description=None):
+    def __init__(self, model, X, y, model_version=None, model_description=None, resource_group_name: str | None = None):
         self.model = model
         self.X = X
         self.y = y
         self.model_version = model_version
         self.model_description = model_description
-        self.container_client = get_data_container_client()
+        self.container_client = get_data_container_client(resource_group_name=resource_group_name)
         self.directory_name = f"explanation_{str(uuid.uuid4())}"
         self.model_metadata = create_model_metadata(model, X, y, model_version=model_version, model_description=model_description)
         self.upload_thread = None
@@ -108,7 +109,7 @@ class ExplainModelContext:
     def _start_upload(self):
         asyncio.run(_upload_blobs_async(self.container_client, self.directory_name, self.X, self.y, self.model, self.model_metadata))
 
-def explain(model, X, y, model_version: str = None, model_description: str = None):
+def explain(model, X, y, model_version: str = None, model_description: str = None, resource_group_name: str | None = None):
     """Upload the model, data, and metadata to the cloudexplaindata container asynchronously.
 
     Usage:
@@ -126,9 +127,10 @@ def explain(model, X, y, model_version: str = None, model_description: str = Non
         y (_type_): _description_
         model_version (str, optional): _description_. Defaults to None.
         model_description (str, optional): _description_. Defaults to None.
+        resource_group_name (str, optional): _description_. Defaults to None.
 
     Returns:
         _type_: _description_
     """
-    return ExplainModelContext(model, X, y, model_version=model_version, model_description=model_description)
+    return ExplainModelContext(model, X, y, model_version=model_version, model_description=model_description, resource_group_name=resource_group_name)
 
